0dae3f9b661a1bd5bb963222f84d903d933d464d
t-tellro <roberto.tellez@globant.com>
2021-02-02 14:35:44 -0600

    Uploading project to github
    First commit for uploading the IssuesTypePredicter project in GitHub.

---
+++ IssuesTypePredicterML.ConsoleApp/IssueData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssuesTypePredicterML.ConsoleApp
+{
+    public class IssueData
+    {
+        public int Number { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# IssuesTypePredicter
+IssuesTypePredicter
